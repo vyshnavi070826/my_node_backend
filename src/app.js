3a821b8792d0f553311803a006151690f5b1aa4b
vyshnavi <vyshnavivulsa07@gmail.com>
2026-03-04 20:49:08 +0530

    serve frontend

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
-const path = require("path");
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const path = require("path");
+
 
 const app = express();
 
