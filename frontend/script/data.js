16005ace0024564165dd91f4e24c2ed0c57ebcfe
vyshnavi <vyshnavivulsa07@gmail.com>
2026-03-04 20:41:23 +0530

    added login signup pages

--- frontend/script/data.js
+++ frontend/script/data.js
@@ -1,7 +1,27 @@
 async function getDepartments() {
-  const response = await fetch("https://my-node-backend-6zba.onrender.com/api/departments");
-  const data = await response.json();
-  console.log(data);
+  try {
+    const response = await fetch("https://my-node-backend-6zba.onrender.com/api/departments");
+    const data = await response.json();
+
+    const grid = document.getElementById("dept-grid");
+
+    grid.innerHTML = "";
+
+    data.forEach((dept) => {
+      const card = document.createElement("div");
+
+      card.className = "p-6 bg-white rounded-xl shadow";
+
+      card.innerHTML = `
+        <h2>${dept.name}</h2>
+      `;
+
+      grid.appendChild(card);
+    });
+
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 getDepartments();
